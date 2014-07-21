33e57bdc401884c69b1b8fb1a6a9867c1c219c6f
Dustin Farris <dustin@dustinfarris.com>
2014-07-21 13:43:27 -0400

    Change host/namespace defaults to use Ember's builtin express server

--- app-addon/initializers/django-adapter.js
+++ app-addon/initializers/django-adapter.js
@@ -1,10 +1,21 @@
 export default {
   name: "django-adapter",
+
   initialize: function(container, app) {
-    var djangoAdapterConfig = {
-      apiHost: app.get('API_HOST') || 'http://localhost:8000',
-      apiNamespace: app.get('API_NAMESPACE') || ''
+
+    var getConfig = function(configKey, defaultValue) {
+      if (typeof app.get(configKey) !== 'undefined') {
+        return app.get(configKey);
+      } else {
+        return defaultValue;
+      }
     };
+
+    var djangoAdapterConfig = {
+      apiHost: getConfig('API_HOST'),
+      apiNamespace: getConfig('API_NAMESPACE', 'api')
+    };
+
     app.register('config:djangoAdapterConfig', djangoAdapterConfig, { instantiate: false });
     app.inject('adapter', 'djangoAdapterConfig', 'config:djangoAdapterConfig');
   }
