341f50e847c3c125cad9b6394effc8736dff1a9e
Dustin Farris <dustin@dustinfarris.com>
2014-07-27 11:55:58 -0400

    Add blueprintsPath to main export

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = {
   name: 'Ember Django Adapter',
+
+  blueprintsPath: function() {
+    return __dirname + '/blueprints';
+  },
+
   included: function(app) {
     app.import({
       development: 'vendor/ember-django-adapter/ember-data-django-rest-adapter.js',
