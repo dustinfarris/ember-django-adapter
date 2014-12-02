13ecdc36a3fb24d84e8b379171c94cbdb7c70e10
Ben Konrath <ben@bagu.org>
2014-12-02 10:10:21 +0100

    Add support for ajax errors.
    The commit relies on the error serialization that was added in Ember
    Data v1.0.0-beta.12.
    
    https://github.com/emberjs/data/commit/4884933497adb4894a3621ad8726cc63c7d910d2

---
+++ tests/integration/crud-failure-test.js
@@ -0,0 +1,145 @@
+import Ember from 'ember';
+import { test } from 'ember-qunit';
+import Pretender from 'pretender';
+import startApp from '../helpers/start-app';
+
+var App;
+var store;
+var server;
+
+
+module('CRUD Failure', {
+  setup: function() {
+    App = startApp();
+
+    store = App.__container__.lookup('store:main');
+
+    server = new Pretender(function() {
+
+      // Permission denied error
+      this.get('/test-api/posts/1/', function(request) {
+        return [403, {'Content-Type': 'application/json'}, JSON.stringify({detail: 'Authentication credentials were not provided.'})];
+      });
+
+      // Server error
+      this.get('/test-api/posts/2/', function(request) {
+        // This is the default error page for Django when DEBUG is set to False.
+        return [500, {'Content-Type': 'text/html'}, '<h1>Server Error (500)</h1>'];
+      });
+
+      // Create field errors
+      this.post('/test-api/posts/', function(request) {
+        return [400, {'Content-Type': 'application/json'}, JSON.stringify({
+          post_title: ['This field is required.'],
+          body: ['This field is required.']
+        })];
+      });
+
+      // Update field errors
+      this.get('/test-api/posts/3/', function(request) {
+        return [200, {'Content-Type': 'application/json'}, JSON.stringify({
+          id: 3,
+          post_title: 'post title 3',
+          body: 'post body 3',
+          comments: []
+        })];
+      });
+      this.put('/test-api/posts/3/', function(request) {
+        return [400, {'Content-Type': 'application/json'}, JSON.stringify({
+          post_title: ['Ensure this value has at most 50 characters (it has 53).'],
+          body: ['This field is required.']
+        })];
+      });
+    });
+  },
+
+  teardown: function() {
+    Ember.run(App, 'destroy');
+    server.shutdown();
+  }
+});
+
+/*
+ * These integration tests need to use the QUnit.stop() / QUnit.start()
+ * pattern as described in the following stackoverflow question:
+ *
+ * https://stackoverflow.com/questions/26317855/ember-cli-how-to-do-asynchronous-model-unit-testing-with-restadapter
+ */
+test('Permission denied error', function() {
+  expect(2);
+
+  stop();
+  Ember.run(function() {
+    store.find('post', 1).then({}, function(response) {
+      ok(response);
+      start();
+      equal(response.toString(), 'Error: Authentication credentials were not provided.');
+    });
+  });
+});
+
+test('Server error', function() {
+  expect(2);
+
+  stop();
+  Ember.run(function() {
+    store.find('post', 2).then({}, function(response) {
+      ok(response);
+      start();
+      equal(response.toString(), 'Error: Internal Server Error');
+    });
+  });
+});
+
+test('Create field errors', function() {
+  expect(6);
+
+  var record,
+    data = {postTitle: '', body: ''};
+
+  stop();
+  Ember.run(function() {
+    record = store.createRecord('post', data);
+    record.save().then({}, function(response) {
+      ok(response);
+      ok(response.errors);
+
+      var errors = response.errors;
+      equal(errors.postTitle.length, 1);
+      equal(errors.postTitle[0], 'This field is required.');
+      equal(errors.body.length, 1);
+      equal(errors.body[0], 'This field is required.');
+
+      start();
+    });
+  });
+});
+
+test('Update field errors', function() {
+  expect(9);
+
+  stop();
+  Ember.run(function() {
+    store.find('post', 3).then(function(response) {
+      ok(response);
+
+      equal(response.get('isDirty'), false);
+      response.set('postTitle', 'Lorem ipsum dolor sit amet, consectetur adipiscing el');
+      response.set('body', '');
+      equal(response.get('isDirty'), true);
+
+      response.save().then({}, function(updateResponse) {
+        ok(updateResponse);
+        ok(updateResponse.errors);
+
+        var errors = updateResponse.errors;
+        equal(errors.postTitle.length, 1);
+        equal(errors.postTitle[0], 'Ensure this value has at most 50 characters (it has 53).');
+        equal(errors.body.length, 1);
+        equal(errors.body[0], 'This field is required.');
+
+        start();
+      });
+    });
+  });
+});
