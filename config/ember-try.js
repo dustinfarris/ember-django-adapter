75d688263a0c0b9339adb89d39d5ed5823d2c98d
Dustin Farris <dustin.farris@gmail.com>
2015-04-14 19:48:32 -0400

    Add `ember try` scenarios
    - ember-data-beta
    - ember-data-canary
    
    Also update `npm test` to only run currently supported Ember Data.

---
+++ config/ember-try.js
@@ -0,0 +1,16 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'ember-data-beta',
+      dependencies: {
+        'ember-data': '1.0.0-beta.16.1'
+      }
+    },
+    {
+      name: 'ember-data-canary',
+      dependencies: {
+        'ember-data': 'canary'
+      }
+    }
+  ]
+};
