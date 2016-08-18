baad318afa31997621ccb9aed820753bb60a8ce6
Dovgan Dmytro <sinled@gmail.com>
2016-08-18 12:25:50 +0300

    Add possibility to use this addon inside addon #163

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ module.exports = {
   name: 'ember-django-adapter',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
 
     app.import('vendor/ember-django-adapter/register-version.js');
   }
