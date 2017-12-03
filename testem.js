3b12254aa80a18b2cc2e18f40c8df4cd1671caa4
Mykola Basov <kolybasov@gmail.com>
2017-12-03 14:42:00 +0200

    Remove Chrome script

--- testem.js
+++ testem.js
@@ -1,24 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome"
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Chrome"
+  launch_in_dev: [
+    'Chrome'
   ],
-  "browser_start_timeout": 60,
-  "browser_args": {
-    "Chrome": [
-      "--disable-gpu",
-      "--disable-web-security", // optional, since chrome 60+
-      "--headless",
-      "--incognito", // optional
-      "--no-sandbox", // optional, since chrome 60+
-      "--remote-debugging-address=0.0.0.0",
-      "--remote-debugging-port=9222"
-    ]
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ]
+    }
   }
 };
