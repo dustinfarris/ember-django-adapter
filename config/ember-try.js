342ef1d4cf9354949dc7b4367a28b41694ce480a
Benjamin Murden <ben.murden@gmail.com>
2018-12-08 21:48:29 +0900

    Try with ember-source in lock-step with data

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,46 +1,72 @@
-/* eslint-env node */
-module.exports = {
-  useYarn: true,
-  scenarios: [
-    {
-      name: 'ember-lts-2.12',
-      npm: {
-        dependencies: {
-          'ember-data': '^2.12.0'
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary')
+  ]).then((urls) => {
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-lts-2.12',
+          npm: {
+            dependencies: {
+              'ember-data': '^2.12.0'
+            },
+            devDependencies: {
+              'ember-source': '^2.12.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-2.16',
+          npm: {
+            dependencies: {
+              'ember-data': '^2.16.0'
+            },
+            devDependencies: {
+              'ember-source': '^2.16.0'
+            }
+          }
+        },
+        {
+          name: 'ember-release',
+          npm: {
+            dependencies: {
+              'ember-data': 'emberjs/data#release'
+            },
+            devDependencies: {
+              'ember-source': urls[0]
+            }
+          }
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            dependencies: {
+              'ember-data': 'emberjs/data#beta'
+            },
+            devDependencies: {
+              'ember-source': urls[1]
+            }
+          }
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            dependencies: {
+              'ember-data': 'emberjs/data#master'
+            },
+            devDependencies: {
+              'ember-source': urls[2]
+            }
+          }
         }
-      }
-    },
-    {
-      name: 'ember-lts-2.16',
-      npm: {
-        dependencies: {
-          'ember-data': '^2.16.0'
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      npm: {
-        dependencies: {
-          'ember-data': 'emberjs/data#release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      npm: {
-        dependencies: {
-          'ember-data': 'emberjs/data#beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      npm: {
-        dependencies: {
-          'ember-data': 'emberjs/data#master'
-        }
-      }
+      ]
     }
-  ]
+  });
 };
